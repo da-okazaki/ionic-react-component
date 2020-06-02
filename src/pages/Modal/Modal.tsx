699c7f9c6ad1a01a0a1af34524eb1df144d4ee95
da-okazaki <da-okazaki@outlook.com>
2020-06-02 09:08:17 +0900

    [clean] : 不要変数 削除

--- src/pages/Modal/Modal.tsx
+++ src/pages/Modal/Modal.tsx
@@ -1,17 +1,12 @@
 import React, { useState, useRef } from 'react';
 import { Redirect, Route } from 'react-router-dom';
 import { 
-  IonApp, IonRouterOutlet, IonPage, IonHeader, IonToolbar, IonTitle, IonContent,
-  IonList, IonListHeader, IonItem, IonLabel, IonInput, IonToggle, IonRadio, IonCheckbox,
-  IonItemSliding, IonItemOptions, IonItemOption, IonAvatar, IonImg, IonCard, IonCardHeader, IonCardContent,
-  IonProgressBar, IonButton, IonIcon, IonButtons, IonBackButton, IonMenuButton, IonCardSubtitle, IonCardTitle,
-  IonGrid, IonCol, IonRow, IonThumbnail, IonInfiniteScroll, IonInfiniteScrollContent, IonModal
+  IonPage, IonHeader, IonToolbar, IonTitle, IonContent,IonButton, IonIcon, IonButtons, IonBackButton, IonModal
 } from '@ionic/react';
-import './Modal.scss';
 import { 
-  heart, heartSharp, chevronBack, playBack, playForward, caretForwardOutline, repeatOutline, heartOutline,
-  share, shareSharp, call, mail, closeOutline, 
+  chevronBack, closeOutline, 
 } from 'ionicons/icons';
+import './Modal.scss';
 import { RouteComponentProps } from 'react-router-dom';
 import { IonReactRouter } from '@ionic/react-router';
 
